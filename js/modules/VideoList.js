75914898aa08cb0ac4a893fe79fd38e384b8c698
Sergey UP <sergey10goals@gmail.com>
2023-10-21 11:14:01 +0300

    fix problem with search video thumbhails

--- js/modules/VideoList.js
+++ js/modules/VideoList.js
@@ -3,10 +3,6 @@ import { Favourite } from "./Favourites";
 
 export class VideoList {
   create(data) {
-    if (data) {
-      this.videoList.innerHTML = '';
-    }
-
     this.videoList = document.querySelector('.video-list__items');
     return this;
   }
@@ -16,7 +12,9 @@ export class VideoList {
       console.log('Не удалось загрузить видео с Youtube, поэтому вы видите демо данные.')
       throw new Error('Sorry, but we have a problem with Youtube server to get data.');
     }
-    console.log("videos=", videos)
+    console.log("Data:", videos)
+
+    this.videoList.innerHTML = '';
 
     new Favourite().set();
     const favouriteVideosId = new Favourite().get();
@@ -65,14 +63,17 @@ export class VideoList {
   }
 
   showSearchVideoList(videos) {
-    console.log("videos=", videos)
+    console.log("Data search:", videos)
+    this.videoList.innerHTML = '';
 
     new Favourite().set();
     const favouriteVideosId = new Favourite().get();
 
     const videoListItems = videos.items.map((
       {
-        id,
+        id: {
+          videoId,
+        },
         snippet: {
           title,
           channelTitle,
@@ -80,11 +81,13 @@ export class VideoList {
         },
       }
     ) => {
+      console.log(title)
+
       const videoListItem = document.createElement('li');
       videoListItem.innerHTML = `
           <article class="video-card">
-          <a class="video-card__link" href="./video.html?id=${id}">
-            <img class="video-card__thumbnail" src="${thumbnails.standard?.url}"
+          <a class="video-card__link" href="./video.html?id=${videoId}">
+            <img class="video-card__thumbnail" src="${thumbnails.high?.url}"
               alt="Превью к видео: ${title}">
             <div class="video-card__info">
               <h3 class="title video-card__title">${title}</h3>
@@ -92,10 +95,10 @@ export class VideoList {
             </div>
           </a>
           <button class="favourite 
-          ${favouriteVideosId.includes(id) ? 'favourite_active' : ''} 
+          ${favouriteVideosId.includes(videoId) ? 'favourite_active' : ''} 
           video-card__favourite" type="button" 
           aria-label="Добавить в Избранное видео: ${title}"
-          data-video-id="${id}">
+          data-video-id="${videoId}">
             <svg class="video-card__favourite-icon" viewBox="0 0 20 20">
               <use xlink:href="./img/sprite.svg#star-fill" />
             </svg>
